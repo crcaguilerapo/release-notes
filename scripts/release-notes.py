6ab97465cd998c2d45a6f307415df3c9f1587faa
criscapunal <crcaguilerapo@unal.edu.co>
2023-11-02 21:52:08 -0500

    test

--- scripts/release-notes.py
+++ scripts/release-notes.py
@@ -115,5 +115,5 @@ changelog = f"""
 
 """
 for folder in ["services/demo1", "services/demo2"]:
-    changelog + "/n" + str (get_commits_by_folder(GITHUB_TOKEN, owner, REPO, pr_number, folder))
+    changelog + "/n" + folder + "/n" + str (get_commits_by_folder(GITHUB_TOKEN, owner, REPO, pr_number, folder))
 create_release(GITHUB_TOKEN, owner, REPO, branch, version, body + changelog)
